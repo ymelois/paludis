fcd020e6d74cb14a527550158ef69c6ba6d06f7b
Youn Mélois <youn@melois.dev>
2026-09-01 19:33:40 +0200

    nix: refactor outputs
    Signed-off-by: Youn Mélois <youn@melois.dev>

--- flake.nix
+++ flake.nix
@@ -10,70 +10,89 @@
   };
 
   outputs =
-    inputs:
+    {
+      self,
+      nixpkgs,
+      eclectic,
+    }:
     let
-      mkPaludis =
-        system: pkgs:
-        pkgs.stdenv.mkDerivation {
-          pname = "paludis";
-          version = "3.0.2";
-          src = ./.;
-
-          buildInputs = [
-            inputs.eclectic.packages."${system}".default
-            pkgs.cmake
-            pkgs.ninja
-            pkgs.gcc
-            pkgs.m4
-            pkgs.doxygen
-            pkgs.jansson.dev
-            pkgs.libarchive.dev
-            pkgs.python3
-            pkgs.ruby
-            pkgs.boost.dev
-            pkgs.file.dev
-            pkgs.gtest
-            pkgs.asciidoc
-            pkgs.xmlto
-            pkgs.html-tidy
-            pkgs.docbook_xml_dtd_45
-          ];
-
-          doCheck = false;
-
-          cmakeFlags = [
-            "-DPALUDIS_CLIENTS=cave"
-            "-DPALUDIS_ENVIRONMENTS=paludis;test"
-            "-DPALUDIS_REPOSITORIES=default;accounts;gemcutter;repository"
-            "-DPALUDIS_DEFAULT_DISTRIBUTION=exherbo"
-            "-DCONFIG_FRAMEWORK=eclectic"
-          ];
-
-          postPatch = ''
-            patchShebangs .
-          '';
-
-          meta = {
-            mainProgram = "cave";
-            license = pkgs.lib.licenses.gpl2;
-          };
-        };
+      forAllSystems = f: builtins.mapAttrs f nixpkgs.legacyPackages;
     in
     {
-      packages = builtins.mapAttrs (system: pkgs: {
-        default = mkPaludis system pkgs;
-        cave = mkPaludis system pkgs;
-      }) inputs.nixpkgs.legacyPackages;
+      packages = forAllSystems (
+        system: pkgs:
+        let
+          cave = pkgs.stdenv.mkDerivation {
+            pname = "paludis";
+            version = "3.0.2";
+            src = ./.;
 
-      app = builtins.mapAttrs (system: pkgs: {
-        default = mkPaludis system pkgs;
-        cave = mkPaludis system pkgs;
-      }) inputs.nixpkgs.legacyPackages;
+            buildInputs = [
+              eclectic.packages."${system}".default
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.gcc
+              pkgs.m4
+              pkgs.doxygen
+              pkgs.jansson.dev
+              pkgs.libarchive.dev
+              pkgs.python3
+              pkgs.ruby
+              pkgs.boost.dev
+              pkgs.file.dev
+              pkgs.gtest
+              pkgs.asciidoc
+              pkgs.xmlto
+              pkgs.html-tidy
+              pkgs.docbook_xml_dtd_45
+            ];
 
-      devShells = builtins.mapAttrs (system: pkgs: {
-        default = pkgs.mkShell {
-          inputsFrom = [ (mkPaludis system pkgs) ];
-        };
-      }) inputs.nixpkgs.legacyPackages;
+            doCheck = false;
+
+            cmakeFlags = [
+              "-DPALUDIS_CLIENTS=cave"
+              "-DPALUDIS_ENVIRONMENTS=paludis;test"
+              "-DPALUDIS_REPOSITORIES=default;accounts;gemcutter;repository"
+              "-DPALUDIS_DEFAULT_DISTRIBUTION=exherbo"
+              "-DCONFIG_FRAMEWORK=eclectic"
+            ];
+
+            postPatch = ''
+              patchShebangs .
+            '';
+
+            meta = {
+              mainProgram = "cave";
+              license = pkgs.lib.licenses.gpl2;
+            };
+          };
+        in
+        {
+          inherit cave;
+          default = cave;
+        }
+      );
+
+      apps = forAllSystems (
+        system: pkgs:
+        let
+          cave = {
+            type = "app";
+            program = pkgs.lib.getExe self.packages.${system}.cave;
+          };
+        in
+        {
+          inherit cave;
+          default = cave;
+        }
+      );
+
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.cave ];
+          };
+        }
+      );
     };
 }
